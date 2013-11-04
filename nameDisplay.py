5d4123c2cdde0b0500d0df58e72d18e57b54a97e
Joel Butcher <joelbutcher97@icloud.com>
2013-11-04 09:38:48 +0000

    added Function File

---
+++ nameDisplay.py
@@ -0,0 +1,11 @@
+def get_first_name ():
+    first_name = input("Please enter your first name: ")
+    return first_name
+
+def get_last_name ():
+    last_name = input("Please enter your last name: ")
+    return last_name
+
+def get_gender ():
+    gender = input("Please enter your gender (M/F): ")
+    return gender
