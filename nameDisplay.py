cdf557443a95cdb3580d239e2c284177b0c52b16
Joel Butcher <joelbutcher97@icloud.com>
2013-11-04 09:55:52 +0000

    Validation
    added while and if validations

--- nameDisplay.py
+++ nameDisplay.py
@@ -8,4 +8,23 @@ def get_last_name ():
 
 def get_gender ():
     gender = input("Please enter your gender (M/F): ")
-    return gender
+    return gender.upper()
+
+def get_details ():
+    valid = False
+    while not valid:
+        first_name = get_first_name()
+        if len(first_name) > 0:
+            valid = True
+
+    valid = False
+    while not valid
+        last_name = get_last_name()
+        if len(last_name) > 0:
+            valid = True
+
+    valid = False
+    while not valid:
+        gender = get_gender()
+        if len(gender) > 0:
+            valid = True
