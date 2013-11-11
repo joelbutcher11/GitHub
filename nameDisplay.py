cb59e133e47f59a3bdb71916b3cecab493a362b8
Joel Butcher <joelbutcher97@icloud.com>
2013-11-11 10:20:52 +0000

    Finished program
    added name_display and edited other functions

--- nameDisplay.py
+++ nameDisplay.py
@@ -1,3 +1,6 @@
+# 04/11/2013
+# Joel Butcher
+
 def get_first_name ():
     first_name = input("Please enter your first name: ")
     return first_name
@@ -7,10 +10,15 @@ def get_last_name ():
     return last_name
 
 def get_gender ():
-    gender = input("Please enter your gender (M/F): ")
+    gender = input("Please enter your gender (m/f): ")
     return gender.upper()
 
 def get_details ():
+    valid = False
+    while not valid:
+        gender = get_gender()
+        if len(gender) > 0:
+            valid = True
     valid = False
     while not valid:
         first_name = get_first_name()
@@ -21,24 +29,32 @@ def get_details ():
         last_name = get_last_name()
         if len(last_name) > 0:
             valid = True
-    valid = False
-    while not valid:
-        gender = get_gender()
-        if len(gender) > 0:
-            valid = True
     return first_name, last_name, gender
 
 def format_name_male (gender, first_name, last_name):
-    return "Mr {0} {1}".format(frist_name, last_name)
+    title_name =  ("Mr {0} {1}".format(first_name, last_name))
+    return title_name
 
 def format_name_female (gender, first_name, last_name):
-    return "Ms {0} {1}".format(frist_name, last_name)
+    title_name = ("Ms {0} {1}".format(first_name, last_name))
+    return title_name
 
 def format_name(first_name, last_name, gender):
     if gender == "M":
-        title_name = format_name_male(first_name, last_name)
+        title_name = format_name_male(gender, first_name, last_name)
+        return title_name
+    elif gender == "F" or gender == "f":
+        title_name = format_name_female(gender, first_name, last_name)
+        return title_name
     else:
-        title_name = format_name_female(fist_name, last_name)
+        ValueError
 
 def output_name(title_name):
     print(title_name)
+
+def name_display():
+    first_name, last_name, gender = get_details()
+    title_name = format_name(first_name, last_name, gender)
+    output_name(title_name)
+
+name_display()
